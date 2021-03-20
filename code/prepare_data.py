7f59a278093975817a1390ae6b43fbd7db8a2204
Yaniv <yanivkman777@gmail.com>
2021-03-21 01:30:34 +0200

    initialize df_all to fix bug of referenced before assignment

--- code/prepare_data.py
+++ code/prepare_data.py
@@ -11,7 +11,7 @@ def load_csvs(path_of_csvs):
     This function receives list of paths to plates' csv files and returns an indexed pandas dataframe of
     all of them. Also this function sets the indecies to the data.
     """
-    df_all: pd.DataFrame
+    df_all: pd.DataFrame = None
     for path in path_of_csvs:
         df_temp: pd.DataFrame = pd.read_csv(path) # The colon is there to indicate the type.
         df_all = pd.concat([df_all, df_temp], ignore_index=True)
