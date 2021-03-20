aa21802c4e06942055ff623e730e7fd35f6c0e67
Yaniv <yanivkman777@gmail.com>
2021-03-20 22:40:51 +0200

    added constants

---
+++ code/constants.py
@@ -0,0 +1,4 @@
+CHANNELS = ["AGP", "DNA", "ER", "Mito", "RNA"]
+LABEL_FIELD = 'Metadata_ASSAY_WELL_ROLE'
+indecies_array = ['Plate', LABEL_FIELD, 'Metadata_broad_sample', 'Image_Metadata_Well',
+                    'ImageNumber', 'ObjectNumber']
